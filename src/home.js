eb754e45f3149e452493b66b878fe077f8d7ca10
Chris Pryor <chrispryor@live.com>
2018-09-01 17:57:23 -0700

    update form validation

--- src/home.js
+++ src/home.js
@@ -2,6 +2,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   M.AutoInit();
   let userInfo = document.querySelector('#userInfo');
+  let clear = document.querySelector('#clear');
   let firstName = document.querySelector('#firstName')
   let lastName = document.querySelector('#lastName');
   let heightFt = document.querySelector('#heightFtInline');
@@ -11,15 +12,31 @@ document.addEventListener('DOMContentLoaded', function(){
   let gender = document.querySelector('#gender');
   let goal = document.querySelector('#goal');
 
+  let fn = localStorage.setItem('First Name', JSON.stringify(firstName.value))
+  let ln = localStorage.setItem('Last Name', JSON.stringify(lastName.value))
+  let ft = localStorage.setItem('Height Ft', JSON.stringify(heightFtInline.value))
+  let inch = localStorage.setItem('Height Inch', JSON.stringify(heightInchInline.value))
+  let wt = localStorage.setItem('Weight', JSON.stringify(weightInline.value))
+  let a = localStorage.setItem('Age', JSON.stringify(age.value))
+  let gen = localStorage.setItem('Gender', JSON.stringify(gender.value))
+  let gl = localStorage.setItem('Goal', JSON.stringify(goal.value))
+
   userInfo.addEventListener('click', function(){
-    localStorage.setItem('First Name', JSON.stringify(firstName.value))
-    localStorage.setItem('Last Name', JSON.stringify(lastName.value))
-    localStorage.setItem('Height Ft', JSON.stringify(heightFtInline.value))
-    localStorage.setItem('Height Inch', JSON.stringify(heightInchInline.value))
-    localStorage.setItem('Weight', JSON.stringify(weightInline.value))
-    localStorage.setItem('Age', JSON.stringify(age.value))
-    localStorage.setItem('Gender', JSON.stringify(gender.value))
-    localStorage.setItem('Goal', JSON.stringify(goal.value))
-    window.location.href = 'fitness.html';
+    if(fn !== undefined && ln !== undefined && ft !== undefined &&
+      inch !== undefined && wt !== undefined && a !== undefined &&
+      gen !== undefined && gl !== undefined){
+      window.location.href = 'fitness.html';
+    }
   });
+
+  clear.addEventListener('click', function(){
+    firstName.value = '';
+    lastName.value = '';
+    heightFt.value = '';
+    heightInch.value = '';
+    weight.value = '';
+    age.value = '';
+    gender.value = '';
+    goal.value = '';
+  })
 })
